3c8dbae518ab06fd8056005a01988e921264b1f0
chidi onuekwusi <chidionuekwusi@gmail.com>
2017-07-24 14:31:14 +0100

    First draft

---
+++ dynamo_mapping.js
@@ -0,0 +1,11 @@
+import components from "./lib";
+const defaultMap = {
+	INPUT: components.dynamo_input,
+	VIEW: components.dynamo_view,
+	CONTAINER: components.dynamo_container,
+	PROCESS: components.dynamo_process,
+	SECTION: components.dynamo_section,
+	SELECT: components.dynamo_select,
+	SELECTSET:components.dynamo_selectset
+};
+export default defaultMap;
---
+++ index.js
@@ -0,0 +1,5 @@
+import mapping from "./dynamo_mapping.js";
+import r from "./lib/reducers";
+
+export default mapping;
+export const reducers = r;
