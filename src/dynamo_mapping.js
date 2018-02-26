e01ef3389d6d4d149da83b14163c1743a5265bd3
chidi onuekwusi <chidionuekwusi@gmail.com>
2018-02-26 11:58:59 +0100

    nav

--- src/dynamo_mapping.js
+++ src/dynamo_mapping.js
@@ -9,15 +9,27 @@ const defaultMap = {
 	SELECTSET: components.dynamo_selectset,
 	LIST: components.dynamo_list,
 	HIDDEN: components.dynamo_hidden,
-	NAV:components.dynamo_nav,
-	IMAGE:components.dynamo_image,
-	GRID:components.dynamo_grid,
-	FILEUPLOAD:components.dynamo_fileupload,
-	ACTIONVIEW:components.dynamo_actionview,
-	HTMLVIEW:components.dynamo_htmlview,
-	LABEL:components.dynamo_label,
-	WEBVIEW:components.dynamo_webview,
-	MESSENGER:components.dynamo_messenger,
-	COMMAND:components.dynamo_command
+	NAV: components.dynamo_nav,
+	IMAGE: components.dynamo_image,
+	GRID: components.dynamo_grid,
+	FILEUPLOAD: components.dynamo_fileupload,
+	ACTIONVIEW: components.dynamo_actionview,
+	HTMLVIEW: components.dynamo_htmlview,
+	LABEL: components.dynamo_label,
+	WEBVIEW: components.dynamo_webview,
+	MESSENGER: components.dynamo_messenger,
+	COMMAND: components.dynamo_command,
+	recipes: {},
+	_defaultMap: {},
+	cook: function() {
+		if (!this._cooked) {
+			this._cooked = true;
+			Object.keys(this.recipes).forEach(recipe => {
+				this._defaultMap[recipe] = this[recipe];
+				this[recipe] = this[recipe].apply(null,this.recipes[recipe]);
+			});
+		}
+		return this;
+	}
 };
 export default defaultMap;
