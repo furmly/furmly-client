8184bb7d60221bc6318e8ad6112874a6171f14d4
chidi onuekwusi <chidionuekwusi@gmail.com>
2018-05-26 01:19:32 +0100

    added try catch transform to render functions

--- src/lib/utils/view.js
+++ src/lib/utils/view.js
@@ -128,86 +128,12 @@ export const toggleAllBusyIndicators = runThroughObj.bind(null, [
 
 export const copy = value => JSON.parse(JSON.stringify(value));
 
-export function safeRender(React, config) {
-	config = config || {};
-	config.errorHandler = config.errorHandler || function() {};
 
-	if (React.hasOwnProperty("unsafeCreateClass")) {
-		return;
-	}
-
-	React.unsafeCreateClass = React.createClass;
-
-	Object.defineProperty(React, "createClass", {
-		get: function() {
-			return createClass;
-		}
-	});
-
-	function createClass(spec) {
-		var componentClass = {};
-
-		function wrap(method, returnFn) {
-			if (!spec.hasOwnProperty(method)) {
-				return;
-			}
-
-			var unsafe = spec[method];
-
-			spec[method] = function() {
-				try {
-					return unsafe.apply(this, arguments);
-				} catch (e) {
-					var report = {
-						displayName: componentClass.displayName,
-						method: method,
-						props: this.props,
-						error: e
-					};
-					if (arguments.length > 0) {
-						report.arguments = arguments;
-					}
-					config.errorHandler(report);
-					return typeof returnFn === "function"
-						? returnFn.apply(this, arguments)
-						: null;
-				}
-			};
-		}
-
-		wrap("render");
-		wrap("componentWillMount");
-		wrap("componentDidMount");
-		wrap("componentWillReceiveProps");
-		wrap("shouldComponentUpdate", safeShouldComponentUpdate);
-		wrap("componentWillUpdate");
-		wrap("componentDidUpdate");
-		wrap("componentWillUnmount");
-		wrap("getInitialState", safeGetInitial);
-
-		// store ref to class in closure so error reporting can be more specific
-		componentClass = React.unsafeCreateClass.apply(React, arguments);
-
-		return componentClass;
-	}
-
-	return React;
-}
-
-function safeShouldComponentUpdate() {
-	return true;
-}
-
-function safeGetInitial() {
-	return {};
-}
 export default {
 	getCurrentStepFromState,
-	safeRender,
 	getTitleFromState,
 	getCurrentStep,
 	getCurrentProcess,
-	safeRender,
 	isValidKey,
 	getKey
 };
